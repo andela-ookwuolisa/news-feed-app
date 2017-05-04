339157cadb968b62e81e3d4118d6eda5476383fa
andela-ookwuolisa <obinna.okwuolisa@andela.com>
2017-05-04 10:32:27 +0100

    add components for heroku

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // create our app
 let app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('Express is up on port 3000');
+app.listen(port, () => {
+  console.log(`Express is up on port ${port}`);
 });
