092839a4b04b6309250f055e2bacb3e0861cc5ec
andela-ookwuolisa <obinna.okwuolisa@andela.com>
2017-05-12 08:03:11 +0100

    test the newsStore

--- app/__tests__/Newsfeeds-test.jsx
+++ app/__tests__/Newsfeeds-test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import expect from 'expect';
+import sinon from 'sinon';
+//import expect from 'expect';
 import Newsfeed from '../components/Newsfeeds';
 
 /*eslint-disable*/
@@ -13,7 +14,48 @@ describe('Test Newsfeeds component', () => {
   it('contains text-center class for h2 header', () => {
     const wrapper = mount(<Newsfeed />);
     expect(wrapper.find('.text-center')).toExist;
+    expect(wrapper.contains(<h2 className="text-center"></h2>).toExist);
+
   });
+
+  it('calls componentDidMount', () => {
+    sinon.spy(Newsfeed.prototype, 'componentDidMount');
+    const wrapper = mount(<Newsfeed />);
+    expect(Newsfeed.prototype.componentDidMount.calledOnce).toEqual(true);
+  });
+  it('calls componentWillUnMount', () => {
+    const spy = sinon.spy();
+
+    class Newsfeed extends React.Component {
+      constructor(props) {
+        super(props);
+        this.componentWillUnmount = spy;
+      }
+      render() {
+        return (
+          <div className={this.props.id}>
+            {this.props.id}
+          </div>
+        );
+      }
+    }
+    const wrapper = shallow(<Newsfeed id="Newsfeed" />);
+    expect(spy.calledOnce).toEqual(false);
+    wrapper.unmount();
+    expect(spy.calledOnce).toEqual(true);
+  });
+
+  it('should render  <div /> component', () => {
+    const wrapper = mount(<Newsfeed />);
+    expect(wrapper.find('div').toExist);
+    expect(wrapper.find('span').toNotExist);
+
+  });
+
+
+
+
+
   xit('it initializes with an array for newsFeeds', () => {
     const wrapper = mount(<Newsfeed />);
     expect(wrapper.node.state.articles).toBeAn('array');
--- app/__tests__/newsStore-test.js
+++ app/__tests__/newsStore-test.js
@@ -2,39 +2,50 @@ import newsStore from '../stores/newsStore';
 import Dispatcher from '../dispatcher/newsDispatcher';
 import expect from 'expect';
 
-let data = {
+const data = {
   title: 'Attempted suicide on Facebook Live has a happy ending',
   description: 'Facebook Live, for all its problems, may have just saved a girl',
 };
 
 jest.mock('../dispatcher/newsDispatcher');
 // jest.dontMock('../stores/newsStore');
-jest.dontMock('object-assign')
+jest.dontMock('object-assign');
 
-  const getNewsObj = {
+const getNewsObj = {
     type: 'DISPLAY_NEWS',
     news: {
-  title: 'Attempted suicide on Facebook Live has a happy ending',
-  description: 'Facebook Live, for all its problems, may have just saved a girl',
-}
+      title: 'Attempted suicide on Facebook Live has a happy ending',
+      description: 'Facebook Live, for all its problems, may have just saved a girl',
+    },
   };
 
-  let callback;
+let callback;
 
-  beforeEach(() => {
+beforeEach(() => {
     callback = Dispatcher.register.mock.calls[0][0];
   });
 
-  test('registers a callback with the dispatcher', () => {
+test('registers a callback with the dispatcher', () => {
     expect(Dispatcher.register.mock.calls.length).toBe(1);
   });
 
-   test('initializes with no data', () => {
-    const count = newsStore.displaySource().length;
-    expect(count).toBe(0);
+test('initializes with no data', () => {
+     const count = newsStore.displaySource().length;
+     expect(count).toBe(0);
+   });
+
+
+test('should call emitChange when "DISPLAY_NEWS" is dispatched', () => {
+    // const spyStore = jest.spyOn(newsStore, 'emitChange');
+      callback(getNewsObj);
+      const count = newsStore.displaySource();
+      const keys = Object.keys(count);
+      expect(keys).toExist();
+    });
+
+test('check if there is an emit change listener method added', () => {
+    expect(newsStore.emitChange).toExist;
   });
-    xit('should call emitChange when "DISPLAY_NEWS" is dispatched', () => {
-    const spyStore = jest.spyOn(newsStore, 'emitChange');
-    callback(getNewsObj);
-    expect(spyStore).toHaveBeenCalled();
-  });
+test('check if there is a remove change listener method', () => {
+  expect(newsStore.removeChangeListener).toExist;
+});
